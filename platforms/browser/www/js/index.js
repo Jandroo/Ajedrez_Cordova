f2f84c033b016f505655d43828a98e07d7e04e45
alejandrooo96 <alejandrooo96@gmail.com>
2018-04-24 19:46:45 +0200

    cambios que tienen que ver con la api

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -17,6 +17,11 @@
  * under the License.
  */
  var token;
+ var contrincante;
+ var texto = $("#texto");
+ var listaUsuarios = $("#listaUsuarios");
+ var invitar = $("#invitarUsuarios");
+
  var app = {
     // Application Constructor
     initialize: function() {
@@ -43,40 +48,35 @@
         console.log('Received Event: ' + id);
         tablero();
 
+        $("#salir").hide();
+        $("#tablero").hide();
         $("form").submit(function(e){
             e.preventDefault();
             var email = $(this).find("input[name='email']").val();
             var password = $(this).find("input[name='password']").val();
 
-            $.getJSON("https://infinite-river-22647.herokuapp.com/api/usuarios/login?email="+email+"&password="+password,function(data) {
-                texto = document.getElementById("texto");
-                if (data.estado == 0){
-                    var node = document.createTextNode(data.mensaje);
-                    texto.appendChild(node);
-                }
-                else{
+            $.getJSON("http://localhost:8000/api/usuarios/login?email="+email+"&password="+password,function(data) {
+                if(data.mensaje == "Session Iniciada"){
+                    texto.text(data.mensaje);
                     token = data.token;
-                    $("p").empty();
-                }
-            }).fail( function(e) {
-                alert("error");
-                console.log(e);
-            });
+                    listaUsuarios.empty();
+                    conectado();
+                    $("#entrar").hide();
+                    $("#salir").show();
+                }else{
+                    texto.text(data.mensaje);
+               }
+           });
         });
 
         $('#boton2').click(function(e){
             e.preventDefault();
-            $.getJSON("https://infinite-river-22647.herokuapp.com/api/usuarios/logout?token="+token,function(data) {
-                texto = document.getElementById("texto");
-                if (data.estado == 0){
-                    var node = document.createTextNode(data.estado);
-                    texto.appendChild(node);
-                }
-                else{
-                    alert(data.estado);
-                    $("p").empty();
-                }
-            })
+            $.getJSON("http://localhost:8000/api/usuarios/logout?token="+token,function(data) {
+             texto.text(data.mensaje);
+             listaUsuarios.empty();
+             $("#entrar").show();
+             $("#salir").hide();
+         })
         });
     }
 };
@@ -85,50 +85,131 @@ var escas, padre, hijo, moviendo;
 
 function tablero(){
 
-    var trebejos0 = ["♖","","","","","","","♖"];
+        rival = "user2";
 
-    escas = document.getElementById("tablero"); 
-    for(f=0; f<8; f++) {
-        var fila = escas.insertRow(); 
+        $.getJSON("http://localhost:8000/api/tablero/ver?token="+token+"&name="+rival,function(data) {
 
-        for(c=0; c<8; c++){
-          var celda = fila.insertCell(); 
-          if(f==0 && c==0) celda.innerHTML = "<span class=negras>"+trebejos0[c]+"</span>"; 
-          else if(f==1) celda.innerHTML = "<span class=>"+"</span>"; 
-          else if(f==6) celda.innerHTML = "<span class=>"+"</span>"; 
-          else if(f==7 && c==7) celda.innerHTML = "<span class=blancas>"+trebejos0[c]+"</span>"; 
+        var trebejos0 = ["♖","","","","","","","♖"];
+
+        var div = document.createElement("div"); 
+        div.setAttribute("id","sombra"); 
+        document.body.appendChild(div); 
+        copia = document.getElementById("sombra"); 
+
+        escas = document.getElementById("tablero"); 
+        for(f=0; f<8; f++) {
+            var fila = escas.insertRow(); 
+
+            for(c=0; c<8; c++){
+              var celda = fila.insertCell(); 
+              if(f==0 && c==0) celda.innerHTML = "<span class=negras>"+trebejos0[c]+"</span>"; 
+              else if(f==1) celda.innerHTML = "<span class=>"+"</span>"; 
+              else if(f==6) celda.innerHTML = "<span class=>"+"</span>"; 
+              else if(f==7 && c==7) celda.innerHTML = "<span class=blancas>"+trebejos0[c]+"</span>"; 
+          }
       }
-  }
 
-  var movible = document.querySelectorAll("td"); 
-  for(m=0; m<movible.length; m++) {
-    movible[m].addEventListener("click", juega);
+      var movible = document.querySelectorAll("td"); 
+      for(m=0; m<movible.length; m++) {
+        $(movible[m]).click(juega);
+    }
+    });
+}
+
+function conectado(){
+    var li = $('<li></li>');
+
+    $.getJSON("http://localhost:8000/api/usuarios/verConectados?token="+token,function(data) {
+        listaUsuarios.append()
+        if(data.usernames === undefined){
+            listaUsuarios.text(data.mensaje);
+        }
+        else{
+            for(var i = 0; i < data.usernames.length; i++){
+               var a = $("<a id='"+data.usernames[i]+"' href='#'>   Invitar</a>").click(function(e){
+                    e.preventDefault();
+                    $.getJSON("http://localhost:8000/api/invitacion/invitar?token="+token+"&name="+e.target.id,function(data) {
+                    console.log(data.mensaje);
+                    })
+                })
+               li.append(data.usernames[i]);
+               li.append(a);
+               listaUsuarios.append(li);
+            }
+        }
+    })
+}
+
+function invitaciones(){
+
+    var li = $('<li></li>');
+
+    $.getJSON("http://localhost:8000/api/invitacion/ver?token="+token,function(data) {
+        for(var i = 0; i < data.mensaje.length; i++){
+
+            var a1 = $("<a id='"+data.mensaje[i].name+"' href='#'>  Aceptar</a>").click(function(e){
+
+                    $.getJSON("http://localhost:8000/api/invitacion/responder?token="+token+"&name="+e.target.id+
+                                "&respuesta=1'",function(data) {
+                    })
+
+                    rival = e.target.id;
+                    $('#tablero').show();
+                    tablero();
+                })
+
+
+            var a2 = $("<a id='"+data.mensaje[i].name+"' href='#'>  Rechazar</a>").click(function(e){
+                    e.preventDefault();
+                    $.getJSON("http://localhost:8000/api/invitacion/responder?token="+token+"&name="+e.target.id+
+                                "&respuesta=0'",function(data) {
+                    })
+                })
+
+            li.append(data.mensaje[i].name);
+            li.append(a1);
+            li.append(a2);
+            invitar.append(li);
+        }
+    });
 }
 
 function juega(T) {
-    var elementos = document.querySelectorAll("table, table span"); 
+    elementos = document.querySelectorAll("table, table span"); 
 
-    if(!moviendo && T.firstElementChild){
+        if(moviendo == null && $(this).find("span")){
+            padre = $(this); 
+            hijo = $(this).html(); 
+            for(i=0; elementos[i]; i++) 
+                elementos[i].classList.add("mano"); 
+            moviendo = this; 
 
-        padre = T; 
-        hijo = T.innerHTML; 
+        }else if(moviendo != null){
+            let to = {
+                fila: $(moviendo).parent().index() + 1,
+                columna: $(moviendo).index() + 1
+            }
 
-        for(i=0; elementos[i]; i++) elementos[i].classList.add("mano"); 
+            let from = {
+                fila: $(this).parent().index() + 1,
+                columna: $(this).index() + 1
+            }
+            console.log(to)
+            $.getJSON("http://localhost:8000/api/tablero/mover?token="+token+"&name="+name+"&toFila="+to.fila+
+        "&toColumna="+to.columna+"&fromFila="+from.fila+"&fromColumna="+from.columna,function(data) {
 
-            T.querySelector("span").style.opacity = ".4"; 
+            console.log(moviendo)
+            if(this != moviendo){
+                $(padre).html("");
+            }
 
-        moviendo = true; 
+            $(this).html(hijo);
+            for(i=0; elementos[i]; i++) 
+                elementos[i].classList.remove("mano");
+            moviendo = null; 
+        
+        })
     }
-
-    else if(moviendo){
-        padre.innerHTML = ""; 
-        T.innerHTML = hijo; 
-
-        for(i=0; elementos[i]; i++) elementos[i].classList.remove("mano"); 
-
-            moviendo = false; 
-    }
-}
 }
 
 app.initialize();
